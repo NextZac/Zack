72d4e928cafbdd85c6cc7a76484da11b57f067a6
NextZac <48414018+NextZac@users.noreply.github.com>
2021-02-03 21:48:06 +0200

    Third Commit

--- assets/js/index.js
+++ assets/js/index.js
@@ -42,11 +42,19 @@ var TxtRotate = function(el, toRotate, period) {
   window.onload = function() {
     var title = document.getElementById("hover")
     title.onmouseover = function() {
-        $('#hover').css("color", 'rgb(201, 0, 44)')
+        $('#hover').css("color", 'rgb(201, 0, 44)');
     }
     title.onmouseleave = function() {
-        $('#hover').css("color", 'wheat')
+        $('#hover').css("color", 'wheat');
     }
+    setTimeout(function(){
+    $('.spinner').fadeOut("slow");
+    }, 1000);
+    setTimeout(function(){
+    $('.fadein').css('display', 'block'); 
+    }, 1500);
+    
+    
     var elements = document.getElementsByClassName('txt-rotate');
     for (var i=0; i<elements.length; i++) {
       var toRotate = elements[i].getAttribute('data-rotate');
