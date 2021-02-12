c345ad1771ed1f8ce1deb1f00c17ab31b9c57402
NextZac <48414018+NextZac@users.noreply.github.com>
2021-02-12 21:56:40 +0200

    aa

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,5 @@
-import * as particlesJS from 'particles.js';
-import { myFirstParticle } from '../../particles.ts'
+import * as particlesJS from 'particlesjs';
+import { myFirstParticle } from './particles.ts'
 
 particlesJS.load('particles-js', myFirstParticle, function() {
   console.log('callback - particles.js config loaded');
